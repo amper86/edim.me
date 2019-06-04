1ec4f8fcad1c81de2fc6bd38176e832668c0ca82
Oleg Mullakaev <o.mullakaev@gmail.com>
2019-06-05 00:31:21 +0300

    section contact & fix html & map add placemarks

--- app/js/map.js
+++ app/js/map.js
@@ -1,22 +1,82 @@
-/*
-ymaps.ready(function(){
-  const myMap = new ymaps.Map('map', {
-    center: [55.76, 37.64],
-    zoom: 10
-  })
-});
-*/
-ymaps.ready(init);
-function init(){
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
+ymaps.ready(() => {
+  const map = new ymaps.Map("map", {
     center: [55.75519455, 37.62768222],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 10
+    controls: ['geolocationControl', 'zoomControl'],
+    zoom: 11,
   });
-}
+  const mitino = new ymaps.Placemark([55.847674, 37.349434], {
+    hintContent: '3-й Митинский переулок, дом 3',
+    balloonContent: 'адрес дописать...'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map_marker_brown.svg',
+    iconImageSize: [30, 60],
+    iconImageOffset: [-15, -50]
+  });
+  const tushino = new ymaps.Placemark([55.828202, 37.439679], {
+    hintContent: 'ул. Тушинская 24с15',
+    balloonContent: 'адрес дописать...'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map_marker_brown.svg',
+    iconImageSize: [30, 60],
+    iconImageOffset: [-15, -50]
+  });
+  const zhivopisnaya = new ymaps.Placemark([55.784435, 37.45707], {
+    hintContent: 'ул. Живописная, дом. 12 корп. 1',
+    balloonContent: 'адрес дописать...'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map_marker_brown.svg',
+    iconImageSize: [30, 60],
+    iconImageOffset: [-15, -50]
+  });
+  const shelkovskoe = new ymaps.Placemark([55.809813, 37.799858], {
+    hintContent: 'Щелковское шоссе, дом 56/72',
+    balloonContent: 'адрес дописать...'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map_marker_brown.svg',
+    iconImageSize: [30, 60],
+    iconImageOffset: [-15, -50]
+  });
+  const sokolniki = new ymaps.Placemark([55.789952, 37.678541], {
+    hintContent: 'Сокольническая площадь 4а.​',
+    balloonContent: 'адрес дописать...'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map_marker_brown.svg',
+    iconImageSize: [30, 60],
+    iconImageOffset: [-15, -50]
+  });
+  const basmanniy = new ymaps.Placemark([55.837778, 37.46149], {
+    hintContent: '1-й Басманный переулок, 5/20с1​',
+    balloonContent: 'адрес дописать...'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map_marker_brown.svg',
+    iconImageSize: [30, 60],
+    iconImageOffset: [-15, -50]
+  });
+  const office = new ymaps.Placemark([55.837778, 37.46149], {
+    hintContent: ' Лодочная улица, 27Ас3​',
+    balloonContent: 'адрес дописать...'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map_marker_red.svg',
+    iconImageSize: [30, 60],
+    iconImageOffset: [-15, -50]
+  });
+
+  map.behaviors.disable(['drag', 'scrollZoom', 'dblClickZoom']);
+
+  map.geoObjects
+    .add(mitino)
+    .add(tushino)
+    .add(zhivopisnaya)
+    .add(shelkovskoe)
+    .add(sokolniki)
+    .add(basmanniy)
+    .add(office);
+});
+//todo: сделать подписи и возможно фото к каждой точке! отцентровать карту...
